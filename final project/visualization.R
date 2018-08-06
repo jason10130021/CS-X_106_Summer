988e9cd12447246aa89b5944099c9eb5f691c2f9
王良軒 <40812819+jason10130021@users.noreply.github.com>
2018-08-06 22:31:22 +0800

    edit animate, over_Q3, visualization

--- final project/visualization.R
+++ final project/visualization.R
@@ -1,54 +1,108 @@
 library(ggplot2)
 library(dplyr)
-#哪種類型最受歡迎 (依年次)
-#各年度bar chart
+library(gridExtra)
+# 種類 VS 特色
+# 1. 種類、特色占比
+# 1.1 十年間種類占比 
+#  bar chart
 ggplot(animate, aes(x= Category, fill=Category))+
   geom_bar()+
   facet_wrap(.~Year)
-#總和pie chart
+#  pie chart
 ggplot(animate, aes(x= factor(1), fill = Category)) +
   geom_bar(width=1)+
   coord_polar(theta = "y")
-#特定年次動畫類型佔比
-year_input<- 2017
-particular_year<- animate%>%
-  filter(Year==year_input)        #以2017為例，後應改為input內容，須注意input後為數字或字串
-#pie chart
-ggplot(particular_year, aes(x=factor(1), fill= Category))+
-  geom_bar(width=1)+
-  coord_polar(theta="y")+
-  labs(title=paste(as.character(year_input), "年動畫類型佔比"))
-#bar chart
-ggplot(particular_year, aes(x=Category, fill= Category))+
+# 1.2 2008-2018 每年人氣前三
+top_3<- data.frame()
+for (i in c(2008:2018)){
+  temp<- animate%>%
+    filter(Year==i)%>%
+      arrange(desc(Popularity))
+  top_3<- rbind(top_3, temp[c(1:3),])
+}
+#將結果建立成清單並存檔
+saveRDS(top_3, "top_3.rds")
+# 1.3 使用top_3清單製作十一年間種類占比
+#  bar chart
+ggplot(top_3, aes(x= Category, fill=Category))+
   geom_bar()+
-  labs(title=paste(as.character(year_input), "年動畫類型佔比"))
-#特定年次動畫人氣比較
-ggplot(particular_year, aes(x=Category, y=Popularity, fill=Category))+
-  geom_col()+
-  labs(title=paste(as.character(year_input), "年動畫類型人氣比較"))
-#指定時段內，動畫...
-year_start<- 2008
-year_end<- 2015     #這邊一樣先自己設值，最後應改為input的
-time_period<- animated%>%
-  filter(Year>=year_start, Year<=year_end)
-#後面就不畫圖，套用前面任何格式圖
+  labs(title=paste("2008-2018年間動畫類型數量"))
+#  pie chart
+ggplot(top_3, aes(x= factor(1), fill = Category)) +
+  geom_bar(width=1)+
+  coord_polar(theta = "y")+
+  labs(title=paste("2008-2018年間動畫類型佔比"))
+# 1.4 使用top_3清單製作十一年間特色占比
+# 先統計出top_3中的特色
+feature_count<- data.frame()
+for (j in c(1:33)){
+  temp<- strsplit(top_3[j,3], split='[、]')[[1]]
+  for (k in c(1:3)){
+    feature_count[3*(j-1)+k,1]<- temp[k]
+  }
+}
+names(feature_count)<- "Feature"
+#  bar chart
+ggplot(feature_count, aes(x= Feature, fill=Feature))+
+  geom_bar()+
+  labs(title=paste("2008-2018年間動畫特色數量"))
+#  pie chart
+ggplot(feature_count, aes(x= factor(1), fill = Feature)) +
+  geom_bar(width=1)+
+  coord_polar(theta = "y")+
+  labs(title=paste("2008-2018年間動畫特色佔比"))
 
-#特定類型各年度人氣變化
-category_input<- animate[2605, 2]
-particular_category<- animate%>%
-  filter(Category==category_input)
-ggplot(particular_category, aes(x=Year, y=Popularity))+
-  geom_col(position="identity")+
-  labs(title=paste(category_input, "類型各年度人氣變化"))
-#各類型年度人氣變化
-ggplot(animate, aes(x=Year, y=Popularity))+
-  geom_col()+
-  facet_wrap(.~Category)
-#各年度類型人氣變化
-ggplot(animate, aes(x=Category, y=log(Popularity), fill=Category))+
-  geom_col()+
-  facet_wrap(.~Year)
+# 2. 人氣指標: 種類 VS 特色
+# 2.1 建立over_Q3清單 (人氣高於該年Q3者)
+over_Q3<- data.frame()
+for (i in c(2008:2018)){
+  anime_20xx<- animate%>%
+    filter(Year==i)
+  summ<- summary(anime_20xx$Popularity)
+  anime_20xx<- anime_20xx%>%
+    filter(Popularity>=summ[5])
+  over_Q3<- rbind(over_Q3, anime_20xx)
+  plot_20xx<- ggplot(anime_20xx, aes(x=(as.Date(MD, format="%m-%d")), y=Popularity, col=Category))+
+    geom_point()+
+    scale_y_continuous(limits = c(0,3000))+
+    geom_hline(aes(yintercept=summ[5]))+
+    labs(title=paste0(i), x="Time")
+  name<- paste0("plot_", i)
+  assign(name, plot_20xx)
+}
+saveRDS(over_Q3, "over_Q3.rds")
 
 
-#2. 每年種類與特色變化 (依人氣)
-#3. 人氣帶動產量 or 產量帶動人氣
+
+# 2.2 每年人氣超過Q3的動畫
+grid.arrange(plot_2008, plot_2009, plot_2010, plot_2011, plot_2012, plot_2013, plot_2014, plot_2015, plot_2016, plot_2017, plot_2018, as.table=FALSE, newpage = FALSE)
+# 單看某一年分
+year<- 2017        #先亂設一個
+anime_20xx<- animate%>%
+  filter(Year==year)
+summ<- summary(anime_20xx$Popularity)
+ggplot(anime_20xx, aes(x=(as.Date(MD, format="%m-%d")), y=Popularity, col=Category))+
+  geom_point()+
+  scale_y_continuous(limits= c(0,3000))+
+  geom_hline(aes(yintercept=summ[5]))+
+  labs(title=year, x="Time")
+# 2.3 人氣高於該年Q3動畫之類型數量比較
+ggplot(over_Q3, aes(x=Category, fill=Category))+
+  geom_bar()+
+  labs(title=paste("人氣高於該年Q3動畫之類型數量比較"))
+# 2.4 建立 over_Q3_feature_count
+over_Q3_feature_count<- data.frame()
+for (k in c(1:738)){
+  temp<- strsplit(over_Q3[k,3], split='[、]')[[1]]
+  for (l in c(1:3)){
+    over_Q3_feature_count[3*(k-1)+l,1]<- temp[l]
+  }
+}
+names(over_Q3_feature_count)<- "Feature"
+saveRDS(over_Q3_feature_count, "over_Q3_feature_count.rds")
+# 2.5 人氣高於該年Q3動畫之特色數量比較 
+ggplot(over_Q3_feature_count, aes(x=Feature, fill=Feature))+
+  geom_bar()+
+  labs(title=paste("人氣高於該年Q3動畫之特色數量比較"))+
+  scale_y_continuous(limits=c(0,500))
+
